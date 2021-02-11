e2a153529b4432387339c71d269c5cbebaf4e5c2
Jason Ou <lloivne4119@gmail.com>
2021-02-12 01:08:56 +0800

    answer_new

--- password_ans.py
+++ password_ans.py
@@ -2,13 +2,16 @@ password = 'a123456'
 x = 3 #剩餘次數
 
 while x > 0 :
+	x = x - 1
 	psw = input('請輸入密碼: ')
 	if psw == password:
 		print('登入成功!')
 		break
 	else:
-		x = x - 1
-		print ('密碼錯誤! 還有', x, '次機會')
+		print ('密碼錯誤!')
+		if x > 0:
+		    print('還有', x, '次機會')
+		else:
+			print('你的帳號被鎖住了啦')
 
-if x == 0:
-    print('你的帳號被鎖住了啦')
+
